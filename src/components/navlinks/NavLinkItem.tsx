87e768a2d137f3a45a665a6378b37a626cf6df0a
Rekhakumari12 <rekhakumari44400@gmail.com>
2023-12-03 12:44:59 +0530

    nav links verticaly center aligned

--- src/components/navlinks/NavLinkItem.tsx
+++ src/components/navlinks/NavLinkItem.tsx
@@ -1,4 +1,5 @@
 import Icon from "components/Icon";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { ICON_TYPES } from "types/type";
 type propType = {
@@ -8,21 +9,27 @@ type propType = {
 };
 const NavLinkItem = (props: propType) => {
   const { path, iconName, label } = props;
-  const getClasses = (isActive: boolean, isTransitioning: boolean) =>
+  const [active, setActive] = useState(false);
+  const getClasses = (isActive: boolean, isTransitioning: boolean): string =>
     [
       isActive ? "text-slate-200" : "text-slate-500",
       isTransitioning ? "transitioning" : "", //todo
-    ].join(" flex");
+    ].join(" flex items-center w-32 h-10");
+
   return (
     <NavLink
       to={path}
-      className={({ isActive, isTransitioning }) =>
-        getClasses(isActive, isTransitioning)
-      }
+      className={({ isActive, isTransitioning }) => {
+        setActive(isActive);
+        return getClasses(isActive, isTransitioning);
+      }}
       end
     >
-      <Icon name={iconName} />
-      <span className="invisible group-hover:visible capitalize">{label}</span>
+      <Icon
+        name={iconName}
+        color={active ? "var(--brand-white)" : "var(--brand-grey)"}
+      />
+      <span className="hidden group-hover:block capitalize ml-2">{label}</span>
     </NavLink>
   );
 };
--- src/components/navlinks/NavbarItems.tsx
+++ src/components/navlinks/NavbarItems.tsx
@@ -3,7 +3,7 @@ import { navLinkData } from "mock/navLinkData";
 
 const NavbarItems = () => {
   return (
-    <nav className="flex flex-col gap-10 m-auto group ">
+    <nav className="flex flex-col gap-10 m-auto group pl-3">
       {navLinkData.map(({ path, iconName, label }) => (
         <NavLinkItem path={path} key={path} iconName={iconName} label={label} />
       ))}
