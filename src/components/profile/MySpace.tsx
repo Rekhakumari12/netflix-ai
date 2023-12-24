a3fad0062c4926ed9824d76c4bfd4a8decf92650
Rekhakumari12 <rekhakumari44400@gmail.com>
2023-12-24 13:19:52 +0530

    add build.yml file

--- src/components/profile/MySpace.tsx
+++ src/components/profile/MySpace.tsx
@@ -1,4 +1,4 @@
-import { Modal } from "components/Modal";
+// import { Modal } from "components/Modal";
 
 const MySpace = () => {
   return (
@@ -8,6 +8,7 @@ const MySpace = () => {
       <button className="bg-gradient-to-r from-blue-700 to-blue-900 text-[--brand-white] py-1 px-3 rounded">
         Login
       </button>
+      <img src="" />
       {/* <Modal /> */}
     </div>
   );
